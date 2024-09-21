60e53bb1cd4b2f67e7cb9211482baff2ef7d09a0
0xFF336699 <0xFF336699@gmail.com>
2024-09-21 09:59:31 +0800

    create docs folder

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@
 
 const nextConfig = {
   output: "export",
+  distDir:"docs"
 };
 
 module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 
-import { pluralMap } from "@0xff336699/js-i18n-lngs";
 import { Container } from "@mui/material";
 import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  console.log(`33333`, pluralMap)
   return (
     // <ChakraProvider theme={theme}>
     <Container>
