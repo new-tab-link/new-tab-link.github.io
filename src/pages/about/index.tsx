2b0cd02e624abd1f726530274404b92f2dc27941
0xFF336699 <0xFF336699@gmail.com>
2024-10-18 22:40:25 +0800

    nojekyll

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,18 +1,9 @@
 
-import { useTranslation } from "react-i18next"
-import i18next from "i18next"
 
 export default function About(){
-
-    const {t} = useTranslation()
-    t('doc/pages/about/content:about.title')
-    t('doc/pages/about/content.about.title')
-    t('content:content.name')
-    i18next
+ 
     return (
         <div>
-            <div>{t('content:name')}</div>
-            <div>{t('content:name', {ns:'doc/pages/about/content'})}</div>
         </div>
     )
 } 
